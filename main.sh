37cb86589cf5c5427596c3aa844d6ead0fb3d63a
mahditofan <165481268+mahditofan@users.noreply.github.com>
2026-02-12 21:59:39 +0330

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,42 +1,58 @@
 #!/bin/bash
 
-TUN_IP_IR="10.20.0.1"
-TUN_IP_KH="10.20.0.2"
-TUN_DEV="gre1"
+TUN_DEV="ipip1"
+IR_TUN_IP="10.50.0.1/30"
+KH_TUN_IP="10.50.0.2/30"
+
+load_module() {
+modprobe ipip
+echo "ipip" >> /etc/modules
+}
+
+enable_forward() {
+echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
+sysctl -p
+}
 
 install_ir() {
 read -p "Enter KHAREJ Public IP: " KH_IP
+LOCAL_IP=$(curl -s ifconfig.me)
 
-ip tunnel add $TUN_DEV mode gre remote $KH_IP local $(curl -s ifconfig.me) ttl 255
-ip addr add $TUN_IP_IR/30 dev $TUN_DEV
-ip link set $TUN_DEV mtu 1400
+load_module
+
+ip tunnel add $TUN_DEV mode ipip remote $KH_IP local $LOCAL_IP
+ip addr add $IR_TUN_IP dev $TUN_DEV
+ip link set $TUN_DEV mtu 1480
 ip link set $TUN_DEV up
 
-echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
-sysctl -p
+enable_forward
 
 echo "IRAN Tunnel Created ✅"
 }
 
 install_kh() {
 read -p "Enter IRAN Public IP: " IR_IP
+LOCAL_IP=$(curl -s ifconfig.me)
 
-ip tunnel add $TUN_DEV mode gre remote $IR_IP local $(curl -s ifconfig.me) ttl 255
-ip addr add $TUN_IP_KH/30 dev $TUN_DEV
-ip link set $TUN_DEV mtu 1400
+load_module
+
+ip tunnel add $TUN_DEV mode ipip remote $IR_IP local $LOCAL_IP
+ip addr add $KH_TUN_IP dev $TUN_DEV
+ip link set $TUN_DEV mtu 1480
 ip link set $TUN_DEV up
 
-echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
-sysctl -p
+enable_forward
 
 echo "KHAREJ Tunnel Created ✅"
 }
 
 status_tunnel() {
-echo "==== Interface Status ===="
+echo "====== Interface ======"
 ip a show $TUN_DEV
-echo "==== Ping Test ===="
-ping -c 4 $TUN_IP_KH
+echo "====== Routing ======"
+ip route
+echo "====== Ping Test ======"
+ping -c 4 10.50.0.2
 }
 
 restart_tunnel() {
@@ -47,12 +63,12 @@ echo "Restarted ✅"
 
 remove_tunnel() {
 ip tunnel del $TUN_DEV
-echo "Tunnel Removed ❌"
+echo "Removed ❌"
 }
 
 menu() {
 clear
-echo "====== tofanTunnel L3 GRE ======"
+echo "====== tofanTunnel L3 IPIP ======"
 echo "1) Install IRAN"
 echo "2) Install KHAREJ"
 echo "3) Status"
