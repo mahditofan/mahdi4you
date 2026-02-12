277950fa745564eba8e6dc6414ab0b927e763f1b
mahditofan <165481268+mahditofan@users.noreply.github.com>
2026-02-12 22:03:58 +0330

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-TUN_DEV="ipip1"
-IR_TUN_IP="10.50.0.1/30"
-KH_TUN_IP="10.50.0.2/30"
-
-load_module() {
-modprobe ipip
-echo "ipip" >> /etc/modules
-}
+VXLAN_DEV="vxlan0"
+VNI="100"
+PORT="443"
+IR_TUN_IP="10.200.0.1/24"
+KH_TUN_IP="10.200.0.2/24"
 
 enable_forward() {
 echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
@@ -18,57 +15,51 @@ install_ir() {
 read -p "Enter KHAREJ Public IP: " KH_IP
 LOCAL_IP=$(curl -s ifconfig.me)
 
-load_module
-
-ip tunnel add $TUN_DEV mode ipip remote $KH_IP local $LOCAL_IP
-ip addr add $IR_TUN_IP dev $TUN_DEV
-ip link set $TUN_DEV mtu 1480
-ip link set $TUN_DEV up
+ip link add $VXLAN_DEV type vxlan id $VNI remote $KH_IP local $LOCAL_IP dstport $PORT dev eth0
+ip addr add $IR_TUN_IP dev $VXLAN_DEV
+ip link set $VXLAN_DEV mtu 1400
+ip link set $VXLAN_DEV up
 
 enable_forward
 
-echo "IRAN Tunnel Created ✅"
+echo "IRAN VXLAN Created ✅"
 }
 
 install_kh() {
 read -p "Enter IRAN Public IP: " IR_IP
 LOCAL_IP=$(curl -s ifconfig.me)
 
-load_module
-
-ip tunnel add $TUN_DEV mode ipip remote $IR_IP local $LOCAL_IP
-ip addr add $KH_TUN_IP dev $TUN_DEV
-ip link set $TUN_DEV mtu 1480
-ip link set $TUN_DEV up
+ip link add $VXLAN_DEV type vxlan id $VNI remote $IR_IP local $LOCAL_IP dstport $PORT dev eth0
+ip addr add $KH_TUN_IP dev $VXLAN_DEV
+ip link set $VXLAN_DEV mtu 1400
+ip link set $VXLAN_DEV up
 
 enable_forward
 
-echo "KHAREJ Tunnel Created ✅"
+echo "KHAREJ VXLAN Created ✅"
 }
 
 status_tunnel() {
 echo "====== Interface ======"
-ip a show $TUN_DEV
-echo "====== Routing ======"
-ip route
+ip a show $VXLAN_DEV
 echo "====== Ping Test ======"
-ping -c 4 10.50.0.2
+ping -c 4 10.200.0.2
 }
 
 restart_tunnel() {
-ip link set $TUN_DEV down
-ip link set $TUN_DEV up
+ip link set $VXLAN_DEV down
+ip link set $VXLAN_DEV up
 echo "Restarted ✅"
 }
 
 remove_tunnel() {
-ip tunnel del $TUN_DEV
+ip link del $VXLAN_DEV
 echo "Removed ❌"
 }
 
 menu() {
 clear
-echo "====== tofanTunnel L3 IPIP ======"
+echo "====== tofanTunnel VXLAN L2 ======"
 echo "1) Install IRAN"
 echo "2) Install KHAREJ"
 echo "3) Status"
